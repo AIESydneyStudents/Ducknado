4440a2840a7b7910e095cfbef1feeb6580e665f3
XYFrenzies <38517312+XYFrenzies@users.noreply.github.com>
2020-10-06 19:18:22 +1100

    I have just fixed a few issues with the last commit. I have also made the player moveable on the main scene

---
+++ project/Assets/Scripts/Player/CameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    [SerializeField] private GameObject player;
+
+    private Vector3 offset;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }//Determines the position of the camera vs the players position to find the middle
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+    //Continually updates the position of the camera so it stays in one position
+}
---
+++ project/Assets/Scripts/Player/TempScripts/CameraScript.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Transform target;
+    [SerializeField] private float distance = 2.0f;
+    [SerializeField] private float xSpeed = 20.0f;
+    [SerializeField] private float ySpeed = 20.0f;
+    [SerializeField] private float yMinLimit = -90f;
+    [SerializeField] private float yMaxLimit = 90f;
+    [SerializeField] private float distanceMin = 10f;
+    [SerializeField] private float distanceMax = 10f;
+    [SerializeField] private float smoothTime = 2f;
+    private float rotationYAxis = 0.0f;
+    private float rotationXAxis = 0.0f;
+    private float velocityX = 0.0f;
+    private float velocityY = 0.0f;
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 angles = transform.eulerAngles;
+        rotationYAxis = angles.y;
+        rotationXAxis = angles.x;
+        // Make the rigid body not change rotation
+        if (GetComponent<Rigidbody>())
+        {
+            GetComponent<Rigidbody>().freezeRotation = true;
+        }
+    }
+    void LateUpdate()
+    {
+        if (target)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                velocityX += xSpeed * Input.GetAxis("Mouse X") * distance * 0.02f;
+                velocityY += ySpeed * Input.GetAxis("Mouse Y") * 0.02f;
+            }
+            rotationYAxis += velocityX;
+            rotationXAxis -= velocityY;
+            rotationXAxis = ClampAngle(rotationXAxis, yMinLimit, yMaxLimit);
+            Quaternion fromRotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);
+            Quaternion toRotation = Quaternion.Euler(rotationXAxis, rotationYAxis, 0);
+            Quaternion rotation = toRotation;
+            distance = Mathf.Clamp(distance - Input.GetAxis("Mouse ScrollWheel") * 5, distanceMin, distanceMax);
+            RaycastHit hit;
+            if (Physics.Linecast(target.position, transform.position, out hit))
+            {
+                distance -= hit.distance;
+            }
+            Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance);
+            Vector3 position = rotation * negDistance + target.position;
+            transform.rotation = rotation;
+            transform.position = position;
+            velocityX = Mathf.Lerp(velocityX, 0, Time.deltaTime * smoothTime);
+            velocityY = Mathf.Lerp(velocityY, 0, Time.deltaTime * smoothTime);
+        }
+    }
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -180F)
+            angle += 180F;
+        if (angle > 180F)
+            angle -= 180F;
+        return Mathf.Clamp(angle, min, max);
+    }
+}
