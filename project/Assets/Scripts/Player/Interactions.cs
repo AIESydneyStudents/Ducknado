0ea15786f0a9d3375c716d5cfd033fdc4c11b201
ben <bennybill16@gmail.com>
2020-10-09 12:35:07 +1100

    I have gotten the ball collision on enter to almost work.

---
+++ project/Assets/Scripts/Player/Interactions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Interactions : MonoBehaviour
+{
+    private Controls controls;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controls = new Controls();
+        controls.Player.Enable();
+        controls.Player.Interaction.performed += Interaction_performed;
+    }
+
+    private void Interaction_performed(InputAction.CallbackContext obj)
+    {
+        controls.Player.Interaction.ReadValue<float>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
