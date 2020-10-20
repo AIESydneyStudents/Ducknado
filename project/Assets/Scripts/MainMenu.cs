a6f7a6b777c1520374ae52f54fe497730036c19b
ben <bennybill16@gmail.com>
2020-10-21 09:45:19 +1100

    I have added a prefab and script for ui
    Ive made it easy so that everyone can use the ui button and customize the button for the changing scenes and quitting the scene.

---
+++ project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class MainMenu : MonoBehaviour
+{
+    public void ChangeScenes(string level)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(level);
+    }
+    public void QuitScene()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+}
