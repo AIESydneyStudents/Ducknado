b256300d7c938f12cc4e02045d8873ed03415bb2
ElricM <elric.milton@yahoo.com.au>
2020-10-21 22:15:31 +1100

    Revamped Menu
    added some buttons, functionality and visuals to menu system

--- project/Assets/Scripts/UI/MainMenu.cs
+++ project/Assets/Scripts/UI/MainMenu.cs
@@ -8,7 +8,8 @@ public class MainMenu : MonoBehaviour
 {
     [SerializeField] GameObject m_mainMenu;//Main Menu gameobject
     [SerializeField] GameObject m_optionsMenu;//Options gameobject
-    [SerializeField] GameObject m_levelSelectMenu;//Options gameobject
+    [SerializeField] GameObject m_levelSelectMenu;//Level Select gameobject
+    [SerializeField] GameObject m_CreditsScreen;//Credits Screen gameobject
 
     public void ChangeScenes(string level)
     {
@@ -26,17 +27,18 @@ public class MainMenu : MonoBehaviour
 
     public void Options() 
     {
-        m_mainMenu.SetActive(false);
         m_optionsMenu.SetActive(true);
+        m_mainMenu.SetActive(false);
         m_levelSelectMenu.SetActive(false);
-
+        m_CreditsScreen.SetActive(false);
     }
 
     public void LevelSelect()
     {
+        m_levelSelectMenu.SetActive(true);
         m_mainMenu.SetActive(false);
         m_optionsMenu.SetActive(false);
-        m_levelSelectMenu.SetActive(true);
+        m_CreditsScreen.SetActive(false);
     }
 
     public void ReturntoMenu() 
@@ -44,7 +46,14 @@ public class MainMenu : MonoBehaviour
         m_mainMenu.SetActive(true);
         m_optionsMenu.SetActive(false);
         m_levelSelectMenu.SetActive(false);
-
+        m_CreditsScreen.SetActive(false);
     }
 
+    public void CreditsScreen()
+    {
+        m_CreditsScreen.SetActive(true);
+        m_mainMenu.SetActive(true);
+        m_optionsMenu.SetActive(false);
+        m_levelSelectMenu.SetActive(false);
+    }
 }
