8dbb335cdc21647a98a699e1c3da4f4d324adf46
Kyle Gian <kyle.gian@hotmail.com>
2020-10-21 15:43:03 +1100

    Added Player Restart & Star rating system
    Player restart works and when they collide with NPC they go back to start pos. Star Rating script added  but is a working progress

---
+++ project/Assets/Scripts/AI/PlayerRestart.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRestart : MonoBehaviour
+{
+    GameObject _NPC;
+    GameObject _player;
+
+    CapsuleCollider sphereCollider;
+
+    Vector3 _playerPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _NPC = GameObject.FindGameObjectWithTag("NPC");
+
+        sphereCollider = _NPC.GetComponent<CapsuleCollider>();
+        _player = this.gameObject;
+
+        _playerPos = _player.transform.position;
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (sphereCollider == other)
+        {
+            Debug.Log("Collision!");
+            this.transform.position = _playerPos;
+        }
+    }
+}
---
+++ project/Assets/Scripts/Tools/StarSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarSystem : MonoBehaviour
+{
+    int _score = 100;
+    GameTimer _gameTimer;
+    NPCTracker _NPCTracker;
+
+    public int _finalScore;
+
+    private void StarRating()
+    {
+        if (_finalScore >= 80 )
+        {
+
+        }
+
+        if (_finalScore >= 50 && _finalScore < 80)
+        {
+
+        }
+        if (_finalScore < 50)
+        {
+
+        }
+    }
+}
