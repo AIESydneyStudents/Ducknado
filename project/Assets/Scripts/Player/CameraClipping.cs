e91d1e721b092965b17b24f40b1a9d097bd8356b
Kyle Gian <kyle.gian@hotmail.com>
2020-10-22 16:26:52 +1100

    Added Camera Clipping Script
    Added a script that will make the object transparent when the camera view is blocked.

---
+++ project/Assets/Scripts/Player/CameraClipping.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraClipping : MonoBehaviour
+{
+    public float _transparency;
+    GameObject _obstruction;
+    Collider _collider;
+    private Material currentMat;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider == _collider)
+        {
+            _collider = _obstruction.GetComponent<Collider>();
+            currentMat = _obstruction.GetComponent<Renderer>().material;
+            ChangeAlpha(_transparency);
+        }
+    }
+
+    void ChangeAlpha(float alphaVal)
+    {
+        Color oldColor = currentMat.color;
+        Color newColor = new Color(oldColor.r, oldColor.g, oldColor.b, alphaVal);
+        currentMat.SetColor("_Color", newColor);
+
+    }
+}
