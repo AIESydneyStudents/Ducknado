d5b4e595b7031fd7c7d51646fc18a5d94320fc81
ben <bennybill16@gmail.com>
2020-10-23 12:21:25 +1100

    I have added a raycast script that allows for a viewing point of shoot.
    Changed the character movement as well but can be fixed later.

--- project/Assets/Scripts/Player/PlayerMovement.cs
+++ project/Assets/Scripts/Player/PlayerMovement.cs
@@ -14,7 +14,9 @@ public class PlayerMovement : MonoBehaviour
     private Controls controls;
     [HideInInspector] public static bool interacted = false;
     public GunController gun;
-    public float weight;
+    [SerializeField] [Range(1.0f, 10.0f)]private float gravity = 2;
+    [SerializeField] private Camera m_cam;
+
     void Start()
     {
 
@@ -22,7 +24,6 @@ public class PlayerMovement : MonoBehaviour
         controls = new Controls();
         controls.Player.Enable();
         controls.Player.Projectile_Swap.performed += Projectile_Swap_performed;
-
 
         //This is for the shooting function of the shooting function.
         //controls.Player.Projectile_Shoot.performed += Projectile_Shoot_performed;
@@ -40,10 +41,13 @@ public class PlayerMovement : MonoBehaviour
     private void Projectile_Swap_performed(InputAction.CallbackContext obj)
     {
         GunController.inHandWeapon += 1;
+        
+        //Determining the visual effect of the line of where the player will be shooting the bullet.
+
     }
     void FixedUpdate()
     {
-        m_playerRB.AddForce(Vector3.down * weight);
+        m_playerRB.AddForce(Vector3.down * gravity);
         var dir = controls.Player.Movement.ReadValue<Vector2>();
         var inter = controls.Player.Interaction.ReadValue<float>();
         var shooting = controls.Player.Projectile_Shoot.ReadValue<float>();
@@ -64,13 +68,23 @@ public class PlayerMovement : MonoBehaviour
                 m_playerRB.velocity = Vector3.ClampMagnitude(m_playerRB.velocity, acceleration);
             }
             //This is for the movement of the player in the certain direction.
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, Camera.main.transform.localEulerAngles.y, transform.localEulerAngles.z);
+
+            //if (dir.x != 0)
+            //{
+            //    Vector3 lookingSide = transform.right * dir.x;
+            //    Vector3 lookingSideNorm = lookingSide.normalized;
+            //    m_playerRB.MoveRotation();
+            //}
+
+            
+
+
             Vector3 input = transform.right * dir.x + transform.forward * dir.y;
             Vector3 inputDir = input.normalized;//Normalises it
             m_playerRB.AddForce(inputDir * playersMS * 10);//Adds velocity to the direction for the player
 
         }
-        //else if (dir.y == 0 && dir.x == 0)//To stop having the velocity 
-        //    m_playerRB.velocity = Vector3.zero;
     }
     
 }
