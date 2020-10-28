6f385acb3cb27706f41b37fdcd76fd707d7b8aa6
Kyle Gian <kyle.gian@hotmail.com>
2020-10-29 10:20:21 +1100

    Sound added to tea placement

--- project/Assets/Scripts/Tools/ObjectiveTracker.cs
+++ project/Assets/Scripts/Tools/ObjectiveTracker.cs
@@ -5,7 +5,6 @@ using TMPro;
 
 public class ObjectiveTracker : MonoBehaviour
 {
-    GameObject[] _objectiveTracker;
     int _objectiveCount;
     [SerializeField] TextMeshProUGUI counterText;
 
@@ -13,9 +12,7 @@ public class ObjectiveTracker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _objectiveTracker = GameObject.FindGameObjectsWithTag("Placement"); //finds the objectives with the tag
-        _objectiveCount = _objectiveTracker.Length; //count is equal to the amount of objectives found
-
+        _objectiveCount = GameObject.FindGameObjectsWithTag("Placement").Length; //finds the objectives with the tag
         counterText.text = _objectiveCount.ToString(); // converts the int into a string 
     }
 }
