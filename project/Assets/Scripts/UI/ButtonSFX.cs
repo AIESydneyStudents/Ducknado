6db03be6a2c51d18deec3fe7755edffe94fd1ae7
ElricM <elric.milton@yahoo.com.au>
2020-10-28 21:02:04 +1100

    Added UI button sounds

---
+++ project/Assets/Scripts/UI/ButtonSFX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSFX : MonoBehaviour
+{
+    public AudioSource fxSource;
+    public AudioClip hoverFx;
+    public AudioClip clickFx;
+
+    public void HoverSound()
+    {
+        fxSource.PlayOneShot(hoverFx);
+    }
+    public void ClickSound()
+    {
+        fxSource.PlayOneShot(clickFx);
+    }
+}
