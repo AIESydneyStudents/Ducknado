c9cc7d6d888b04120d076be8252b8c35bd36348a
Elric <elric.milton@yahoo.com.au>
2020-11-05 11:28:09 +1100

    Added hiding mechanic

---
+++ project/Assets/Scripts/Player/HidingCheck.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HidingCheck : MonoBehaviour
+{
+    float collisionRadius;
+    void Start()
+    {
+        collisionRadius = gameObject.GetComponent<CapsuleCollider>().radius;
+    }
+
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Hiding Area")
+        {
+            gameObject.transform.GetChild(0).gameObject.SetActive(false);
+            gameObject.transform.GetChild(1).gameObject.SetActive(true);
+            gameObject.transform.GetChild(2).gameObject.SetActive(false);
+            collisionRadius = .15f;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Hiding Area")
+        {
+            gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            gameObject.transform.GetChild(1).gameObject.SetActive(false);
+            gameObject.transform.GetChild(2).gameObject.SetActive(true);
+            collisionRadius = .5f;
+        }
+    }
+}
