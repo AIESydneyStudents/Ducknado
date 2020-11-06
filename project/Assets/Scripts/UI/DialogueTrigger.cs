4280b3ac862fa225ee8f998c8edf46d76e622dce
ben <bennybill16@gmail.com>
2020-11-06 14:54:31 +1100

    I have added a new prefab which is for the textbox UI.
    This is called Dialogue Canvas

--- project/Assets/Scripts/UI/DialogueTrigger.cs
+++ project/Assets/Scripts/UI/DialogueTrigger.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
+    [SerializeField] public GameObject setActiveText;
     public Dialogue dialogue;
-
+    private bool hasBeenPlayed = false;
     public void TriggerDialogue()
     {
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && hasBeenPlayed == false)
+        {
+            setActiveText.SetActive(true);
+            FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+            hasBeenPlayed = true;
+        }
+
     }
 }
