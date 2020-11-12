f2e6fe7012842241e4fa1898a65bd3d3f7a26292
ben <bennybill16@gmail.com>
2020-11-12 11:04:43 +1100

    Pushing up a bug that i had with the camera where the material is null.

--- project/Assets/Scripts/Player/CameraClipping.cs
+++ project/Assets/Scripts/Player/CameraClipping.cs
@@ -7,19 +7,9 @@ public class CameraClipping : MonoBehaviour
     [HideInInspector] public List<MeshRenderer> listobj;
     [HideInInspector] public List<Material> objectMaterials;
     [SerializeField] public Material alphaMat;
-    
-
-
-    private void Update()
-    {
-        RayCastSeeThrough();
-    }
-
-    void RayCastSeeThrough()
+    private void FixedUpdate()
     {
         RaycastHit hit;
-
-
         if (Physics.Raycast(Camera.main.gameObject.transform.position,
             Camera.main.gameObject.transform.forward, out hit, 6) &&//Needs to be adjustable not have 6 as its parameter.
             !hit.collider.gameObject.CompareTag("Player"))
@@ -27,9 +17,12 @@ public class CameraClipping : MonoBehaviour
             //Saves memory space as there will be less variables to check through a list.
             MeshRenderer objectMesh = hit.transform.gameObject.GetComponent<MeshRenderer>();
             objectMesh.GetComponent<Material>();
-            if (objectMesh.material.color.a != alphaMat.color.a && objectMesh.gameObject.CompareTag("Wall"))//Because this is being called in update, it is always being called.
+            if (alphaMat != null)
             {
-                AddToList(objectMesh);
+                if (objectMesh.material.color.a != alphaMat.color.a && objectMesh.gameObject.CompareTag("Wall"))//Because this is being called in update, it is always being called.
+                {
+                    AddToList(objectMesh);
+                }
             }
         }
         else if (Physics.Raycast(Camera.main.gameObject.transform.position,
@@ -38,6 +31,7 @@ public class CameraClipping : MonoBehaviour
         {
             SetBack();
         }
+
     }
     void SetBack()//Setting the meshRenderer back to active
     {
