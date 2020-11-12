f0650e3c690eec195e8d645c6ace11b7a33879ca
ben <bennybill16@gmail.com>
2020-11-12 11:31:45 +1100

    Creating an Items script

---
+++ project/Assets/Scripts/Player/ItemsInGame.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemsInGame : MonoBehaviour
+{
+    public class itemsToGame
+    {
+        public List<GameObject> itemInGame;
+    }
+    [SerializeField] public List<itemsToGame> itemsAvailable;
+    private List<GameObject> itemsInList;
+    [HideInInspector] public int objectsInGame;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        objectsInGame = itemsAvailable.Count;
+        foreach (itemsToGame item in itemsAvailable)
+        {
+            for (int i = 0; i < item.itemInGame.Count; i++)
+            {
+
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+        }
+    }
+}
---
+++ project/Assets/Scripts/Player/PickupItems.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PickupItems : MonoBehaviour
+{
+    [SerializeField] private GameObject item;
+    [SerializeField] private Text textOfItem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            item.SetActive(false);
+
+        }
+    }
+}
