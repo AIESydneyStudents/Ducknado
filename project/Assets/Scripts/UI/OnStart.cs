964ecb6c2ed09d076a97065375251a6dfab93076
Ben <bennybill16@gmail.com>
2020-11-17 15:15:31 +1100

    Testing OnStart Script for the transitioning of the scenes

--- project/Assets/Scripts/UI/OnStart.cs
+++ project/Assets/Scripts/UI/OnStart.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class OnStart : MonoBehaviour
 {
+    [SerializeField] public DialogueTrigger dialogue;
+    [SerializeField] public GameObject textBoxDialogue;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        //Time.timescale
+        Time.timeScale = 0f;
+        textBoxDialogue.SetActive(true);
+        dialogue.TriggerDialogue();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        FadeOutScreen fading = GetComponent<FadeOutScreen>();
+        if (fading._fadeOut == true)
+            Time.timeScale = 1f;
     }
 }
