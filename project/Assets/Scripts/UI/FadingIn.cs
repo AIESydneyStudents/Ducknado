151f0ac430cd883c209ecbdcbf1d1aecb5db5858
ben <bennybill16@gmail.com>
2020-11-18 16:50:28 +1100

    Fixed Up the level select for the pause menu.

--- project/Assets/Scripts/UI/FadingIn.cs
+++ project/Assets/Scripts/UI/FadingIn.cs
@@ -15,7 +15,7 @@ public class FadingIn : MonoBehaviour
     void Awake()
     {
         SharedInstance = this;
-        fadingPanel.gameObject.SetActive(true);
+        fadingPanel.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -23,6 +23,7 @@ public class FadingIn : MonoBehaviour
     {
         if (fadingIn == true)
         {
+            fadingPanel.gameObject.SetActive(true);
             fadingPanel.color = new Color(0, 0, 0, _alphaIn += 0.5f * Time.unscaledDeltaTime);
             fadingOut = false;
             if (fadingPanel.color.a >= 255)
