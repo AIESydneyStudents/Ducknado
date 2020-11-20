a47036e124c00ab2d7686f25aa18d5e04705991c
ben <bennybill16@gmail.com>
2020-11-20 11:19:01 +1100

    Fixed the end game where the mouse isnt in the middle and the walls

--- project/Assets/Scripts/UI/PauseMenu.cs
+++ project/Assets/Scripts/UI/PauseMenu.cs
@@ -8,6 +8,7 @@ public class PauseMenu : MonoBehaviour
     [SerializeField] private GameObject pauseMenu;
     [SerializeField] private GameObject gameMenu;
     [SerializeField] private GameObject fadingPanel;
+    public static bool isFinished = false;
     private bool isPaused = false;
     private bool buttonPressed = false;
     // Start is called before the first frame update
@@ -20,7 +21,7 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.Escape))
+        if (Input.GetKey(KeyCode.Escape) && isFinished == false)
         {
             if (!isPaused)
             {
