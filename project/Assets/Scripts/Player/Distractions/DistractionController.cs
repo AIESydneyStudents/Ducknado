308ed9df4817b60830df34a9d48fea546f057430
ben <bennybill16@gmail.com>
2020-11-26 15:06:00 +1100

    I have adjusted a few files and scripts and added a fps.

--- project/Assets/Scripts/Player/Distractions/DistractionController.cs
+++ project/Assets/Scripts/Player/Distractions/DistractionController.cs
@@ -16,20 +16,15 @@ public class DistractionController : MonoBehaviour
     {
         if (isFloating)//If the bullet is firing.
         {
-            if (ProjectileController.keyIsReleased == false)//Change this back to true
+            GameObject fairyBullet = objPooling.SharedInstance.GetPooledObject("FairyBull");// Gets a Fairy that is not active
+            if (fairyBullet != null)
             {
-                GameObject fairyBullet = objPooling.SharedInstance.GetPooledObject("FairyBull");// Gets a Fairy that is not active
-                if (fairyBullet != null)
-                {
-                    fairyBullet.transform.position = projectilePoint.position;
-                    fairyBullet.transform.rotation = projectilePoint.transform.rotation;
-                    fairyBullet.SetActive(true);
-                    objPooling.SharedInstance.RemoveObjects(fairyBullet);
-                }
-                isFloating = false;
-
+                fairyBullet.transform.position = projectilePoint.position;
+                fairyBullet.transform.rotation = projectilePoint.transform.rotation;
+                fairyBullet.SetActive(true);
+                objPooling.SharedInstance.RemoveObjects(fairyBullet);
             }
-
+            isFloating = false;
         }
 
     }
